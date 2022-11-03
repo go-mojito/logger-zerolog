aacb0f454416db6518a997d66d3bb8527b96f338
Cedric Lewe <0SkillAllLuck@pm.me>
2022-11-04 00:01:11 +0100

    Add SetOutput to Logger (#16)
    * Add SetOutput to Logger
    
    * Fix go.mod

--- go.mod
+++ go.mod
@@ -1,9 +1,9 @@
 module github.com/go-mojito/logger-zerolog
 
-go 1.18
+go 1.19
 
 require (
-	github.com/go-mojito/mojito v0.0.13
+	github.com/go-mojito/mojito v0.0.14
 	github.com/rs/zerolog v1.28.0
 )
 
@@ -11,9 +11,10 @@ require (
 	github.com/infinytum/injector v0.0.2 // indirect
 	github.com/infinytum/structures v0.0.2 // indirect
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
-	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mitchellh/hashstructure/v2 v2.0.2 // indirect
-	golang.org/x/net v0.0.0-20220906165146-f3363e06e74c // indirect
-	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+	golang.org/x/net v0.0.0-20220909164309-bea034e7d591 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- logger.go
+++ logger.go
@@ -2,6 +2,7 @@ package zerolog
 
 import (
 	"fmt"
+	"io"
 
 	"github.com/go-mojito/mojito/pkg/logger"
 	"github.com/rs/zerolog"
@@ -10,119 +11,129 @@ import (
 
 // Logger is a wrapper around zerolog.Logger
 type Logger struct {
+	logger zerolog.Logger
 	fields logger.Fields
 }
 
 // NewLogger returns a new Logger
-func NewLogger() Logger {
-	return Logger{
+func NewLogger() *Logger {
+	return &Logger{
+		logger: log.Logger,
 		fields: make(logger.Fields),
 	}
 }
 
 // NewLogger returns a new Logger
-func NewLoggerWithFields(fields logger.Fields) Logger {
-	return Logger{
+func NewLoggerWithFields(logger zerolog.Logger, fields logger.Fields) *Logger {
+	return &Logger{
+		logger: logger,
 		fields: fields,
 	}
 }
 
-// Debug will write a debug log
-func (l Logger) Debug(msg interface{}) {
-	log := log.Debug()
-	log = l.addFields(log)
-	log.Msg(fmt.Sprintf("%s", msg))
-}
-
-// Debugf will write a debug log sprintf-style
-func (l Logger) Debugf(msg string, values ...interface{}) {
-	log := log.Debug()
-	log = l.addFields(log)
-	log.Msgf(msg, values...)
-}
-
-// Error will write a error log
-func (l Logger) Error(msg interface{}) {
-	log := log.Error()
-	log = l.addFields(log)
-	log.Msg(fmt.Sprintf("%s", msg))
-}
-
-// Errorf will write a error log sprintf-style
-func (l Logger) Errorf(msg string, values ...interface{}) {
-	log := log.Error()
-	log = l.addFields(log)
-	log.Msgf(msg, values...)
-}
-
-// Fatal will write a fatal log
-func (l Logger) Fatal(msg interface{}) {
-	log := log.Fatal()
-	log = l.addFields(log)
-	log.Msg(fmt.Sprintf("%s", msg))
-}
-
-// Fatalf will write a fatal log sprintf-style
-func (l Logger) Fatalf(msg string, values ...interface{}) {
-	log := log.Fatal()
-	log = l.addFields(log)
-	log.Msgf(msg, values...)
+// SetOutput sets the output destination for the default logger.
+func (l *Logger) SetOutput(w io.Writer) error {
+	logger := l.logger.Output(w)
+	l.logger = logger
+	return nil
 }
 
 // Field will add a field to a new logger and return it
-func (l Logger) Field(name string, val interface{}) logger.Logger {
+func (l *Logger) Field(name string, val interface{}) logger.Logger {
 	f := l.fields.Clone()
 	f[name] = val
-	return NewLoggerWithFields(f)
+	return NewLoggerWithFields(l.logger, f)
 }
 
 // Fields will add multiple fields to a new logger and return it
-func (l Logger) Fields(fields logger.Fields) logger.Logger {
+func (l *Logger) Fields(fields logger.Fields) logger.Logger {
 	f := l.fields.Clone()
 	for name, val := range fields {
 		f[name] = val
 	}
-	return NewLoggerWithFields(f)
-}
-
-// Info will write a info log
-func (l Logger) Info(msg interface{}) {
-	log := log.Info()
-	log = l.addFields(log)
-	log.Msg(fmt.Sprintf("%s", msg))
-}
-
-// Infof will write a info log sprintf-style
-func (l Logger) Infof(msg string, values ...interface{}) {
-	log := log.Info()
-	log = l.addFields(log)
-	log.Msgf(msg, values...)
+	return NewLoggerWithFields(l.logger, f)
 }
 
 // Trace will write a trace log
-func (l Logger) Trace(msg interface{}) {
-	log := log.Trace()
+func (l *Logger) Trace(msg interface{}) {
+	log := l.logger.Trace()
 	log = l.addFields(log)
 	log.Msg(fmt.Sprintf("%s", msg))
 }
 
 // Tracef will write a trace log sprintf-style
-func (l Logger) Tracef(msg string, values ...interface{}) {
-	log := log.Trace()
+func (l *Logger) Tracef(msg string, values ...interface{}) {
+	log := l.logger.Trace()
+	log = l.addFields(log)
+	log.Msgf(msg, values...)
+}
+
+// Debug will write a debug log
+func (l *Logger) Debug(msg interface{}) {
+	log := l.logger.Debug()
+	log = l.addFields(log)
+	log.Msg(fmt.Sprintf("%s", msg))
+}
+
+// Debugf will write a debug log sprintf-style
+func (l *Logger) Debugf(msg string, values ...interface{}) {
+	log := l.logger.Debug()
+	log = l.addFields(log)
+	log.Msgf(msg, values...)
+}
+
+// Info will write a info log
+func (l *Logger) Info(msg interface{}) {
+	log := l.logger.Info()
+	log = l.addFields(log)
+	log.Msg(fmt.Sprintf("%s", msg))
+}
+
+// Infof will write a info log sprintf-style
+func (l *Logger) Infof(msg string, values ...interface{}) {
+	log := l.logger.Info()
 	log = l.addFields(log)
 	log.Msgf(msg, values...)
 }
 
 // Warn will write a warn log
-func (l Logger) Warn(msg interface{}) {
-	log := log.Warn()
+func (l *Logger) Warn(msg interface{}) {
+	log := l.logger.Warn()
 	log = l.addFields(log)
 	log.Msg(fmt.Sprintf("%s", msg))
 }
 
 // Warnf will write a warn log sprintf-style
-func (l Logger) Warnf(msg string, values ...interface{}) {
-	log := log.Warn()
+func (l *Logger) Warnf(msg string, values ...interface{}) {
+	log := l.logger.Warn()
+	log = l.addFields(log)
+	log.Msgf(msg, values...)
+}
+
+// Error will write a error log
+func (l *Logger) Error(msg interface{}) {
+	log := l.logger.Error()
+	log = l.addFields(log)
+	log.Msg(fmt.Sprintf("%s", msg))
+}
+
+// Errorf will write a error log sprintf-style
+func (l *Logger) Errorf(msg string, values ...interface{}) {
+	log := l.logger.Error()
+	log = l.addFields(log)
+	log.Msgf(msg, values...)
+}
+
+// Fatal will write a fatal log
+func (l *Logger) Fatal(msg interface{}) {
+	log := l.logger.Fatal()
+	log = l.addFields(log)
+	log.Msg(fmt.Sprintf("%s", msg))
+}
+
+// Fatalf will write a fatal log sprintf-style
+func (l *Logger) Fatalf(msg string, values ...interface{}) {
+	log := l.logger.Fatal()
 	log = l.addFields(log)
 	log.Msgf(msg, values...)
 }
