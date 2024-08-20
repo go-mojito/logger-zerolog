4a1698d3685c3d654134fb67b6165c013b03a765
NilaTheDragon <nilathedragon@pm.me>
2024-08-20 16:28:33 +0200

    Try to not use .Logger until we log

--- logger.go
+++ logger.go
@@ -11,19 +11,19 @@ import (
 
 // Logger is a wrapper around zerolog.Logger
 type Logger struct {
-	logger zerolog.Logger
+	logger zerolog.Context
 }
 
 // NewLogger returns a new Logger
 func NewLogger() *Logger {
 	return &Logger{
-		logger: log.Logger.With().Logger(),
+		logger: log.Logger.With(),
 	}
 }
 
 // SetOutput sets the output destination for the current logger.
 func (l *Logger) SetOutput(w io.Writer) error {
-	logger := l.logger.Output(w)
+	logger := l.logger.Logger().Output(w).With()
 	l.logger = logger
 	return nil
 }
@@ -31,73 +31,78 @@ func (l *Logger) SetOutput(w io.Writer) error {
 // Field will add a field to a new logger and return it
 func (l *Logger) Field(name string, val interface{}) logger.Logger {
 	return &Logger{
-		logger: l.logger.With().Interface(name, val).Logger(),
+		logger: l.logger.Interface(name, val),
 	}
 }
 
 // Fields will add multiple fields to a new logger and return it
 func (l *Logger) Fields(fields logger.Fields) logger.Logger {
 	return &Logger{
-		logger: l.logger.With().Fields(map[string]interface{}(fields)).Logger(),
+		logger: l.logger.Fields(map[string]interface{}(fields)),
 	}
 }
 
 // Trace will write a trace log
 func (l *Logger) Trace(msg interface{}) {
-	l.logger.Trace().Msg(fmt.Sprintf("%s", msg))
+	l.toLogger().Trace().Msg(fmt.Sprintf("%s", msg))
 }
 
 // Tracef will write a trace log sprintf-style
 func (l *Logger) Tracef(msg string, values ...interface{}) {
-	l.logger.Trace().Msgf(msg, values...)
+	l.toLogger().Trace().Msgf(msg, values...)
 }
 
 // Debug will write a debug log
 func (l *Logger) Debug(msg interface{}) {
-	l.logger.Debug().Msg(fmt.Sprintf("%s", msg))
+	l.toLogger().Debug().Msg(fmt.Sprintf("%s", msg))
 }
 
 // Debugf will write a debug log sprintf-style
 func (l *Logger) Debugf(msg string, values ...interface{}) {
-	l.logger.Debug().Msgf(msg, values...)
+	l.toLogger().Debug().Msgf(msg, values...)
 }
 
 // Info will write a info log
 func (l *Logger) Info(msg interface{}) {
-	l.logger.Info().Msg(fmt.Sprintf("%s", msg))
+	l.toLogger().Info().Msg(fmt.Sprintf("%s", msg))
 }
 
 // Infof will write a info log sprintf-style
 func (l *Logger) Infof(msg string, values ...interface{}) {
-	l.logger.Info().Msgf(msg, values...)
+	l.toLogger().Info().Msgf(msg, values...)
 }
 
 // Warn will write a warn log
 func (l *Logger) Warn(msg interface{}) {
-	l.logger.Warn().Msg(fmt.Sprintf("%s", msg))
+	l.toLogger().Warn().Msg(fmt.Sprintf("%s", msg))
 }
 
 // Warnf will write a warn log sprintf-style
 func (l *Logger) Warnf(msg string, values ...interface{}) {
-	l.logger.Warn().Msgf(msg, values...)
+	l.toLogger().Warn().Msgf(msg, values...)
 }
 
 // Error will write a error log
 func (l *Logger) Error(msg interface{}) {
-	l.logger.Error().Msg(fmt.Sprintf("%s", msg))
+	l.toLogger().Error().Msg(fmt.Sprintf("%s", msg))
 }
 
 // Errorf will write a error log sprintf-style
 func (l *Logger) Errorf(msg string, values ...interface{}) {
-	l.logger.Error().Msgf(msg, values...)
+	l.toLogger().Error().Msgf(msg, values...)
 }
 
 // Fatal will write a fatal log
 func (l *Logger) Fatal(msg interface{}) {
-	l.logger.Fatal().Msg(fmt.Sprintf("%s", msg))
+	l.toLogger().Fatal().Msg(fmt.Sprintf("%s", msg))
 }
 
 // Fatalf will write a fatal log sprintf-style
 func (l *Logger) Fatalf(msg string, values ...interface{}) {
-	l.logger.Fatal().Msgf(msg, values...)
+	l.toLogger().Fatal().Msgf(msg, values...)
+}
+
+func (l *Logger) toLogger() *zerolog.Logger {
+	logger := l.logger.Logger()
+	return &logger
 }
